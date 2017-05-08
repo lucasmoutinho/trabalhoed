b3d8be1aec1906066d1b7ea67caad0e6fdb60e06
Lucas Moutinho <lucasmoutinho>
2017-05-07 22:04:26 -0300

    add func vetor.c

---
+++ vetor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* criaVetor(int n){
+	char* vetor = (char*)malloc(n*sizeof(char));
+	return vetor;
+}
+
+int main(){
+	char* infixa;
+	int i = 0,tam;
+	infixa = criaVetor(151);
+	scanf("%s", infixa);
+  	infixa = (char*)realloc(infixa,(((int)strlen(infixa)+1)*sizeof(char)));
+  	printf("%d\n", (int)strlen(infixa));
+  	while(infixa[i] != '\0'){
+  		printf("%c ", infixa[i]);
+  		i++;
+  	}
+	return 0;
+}
+
